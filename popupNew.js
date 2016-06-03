99b5bdd8dfaa04fa16e44ed1c551d0a36144c197
rbarcklay <rachel.barcklay@gmail.com>
2016-06-03 10:51:44 -0700

    color graphs, top ten sites
    funky scroll over tho

--- popupNew.js
+++ popupNew.js
@@ -3,39 +3,81 @@
  */
 
 var dataRef = new Firebase('https://browsinghistory.firebaseio.com/');
-var d;
+var data;
+
+function sortData(arr) {
+    arr.sort(function(a, b) {
+        return b.time - a.time;
+    });
+    return arr;
+};
+
+//gets top ten websites from all browsing history
+function tenSites(arr) {
+    var sortedTen = sortData(arr).slice(0,10);
+    var sites = [];
+    for (i in sortedTen) {
+        sites.push(sortedTen[i].label);
+    }
+    return sites;
+};
+
+function tenTimes(arr) {
+    var sortedTen = sortData(arr).slice(0,10);
+    var times = [];
+    for (i in sortedTen) {
+        times.push(sortedTen[i].time);
+    }
+    return times;
+};
 
 var main =function () {
 
     var ctx= document.getElementById("chromeData").getContext("2d");
     var pieData = {};
+    var tempData = [];
 
     dataRef.on("value", function (snapshot) {
 
-        d = snapshot.val();
+        data = snapshot.val();
+        //var tempData = [];
 
-        var l = [];
-        var dat = [];
-
-        var siteD = d['websites'];
+        var siteD = data['websites'];
         for (i in siteD) {
-
-            l.push(i.split("_").join("."));
 
             var time = 0;
             for (j in siteD[i]) {
                 time += parseInt(siteD[i][j]);
             }
-            dat.push(time);
+
+            var label = i.split("_").join(".");
+            tempData.push({label: label, time: time});
         }
+
+        var l = tenSites(tempData);
+        var d = tenTimes(tempData);
+
         pieData = {
             labels: l,
             datasets: [
                 {
-                    data: dat
+                    data: d,
+                    backgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56",
+                        "#99ff99",
+                        "#9999ff",
+                        "#ff66cc",
+                        "#0066cc",
+                        "#ffff99",
+                        "#79d279",
+                        "#cc6699"
+                    ]
                 }]
         }
-        console.log(pieData);
+
+        //console.log(pieData);
 
         new Chart(ctx, {type: "pie", data: pieData});
 
